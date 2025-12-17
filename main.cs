42eb20f7bf2e4189cd05208ad98a783c115eaf62
Gus <guspearson78@gmail.com>
2025-12-16 16:30:01 -0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,5 +2,43 @@ using Octokit;
 
 namespace main
 {
-    var app = 
+    public class main
+    {
+        //import Github API
+        //https://octokitnet.readthedocs.io/en/latest/
+        //https://dotnet.microsoft.com/en-us/apps/aspnet/apis
+        var responses = new [] {"-r", "-u", "Q"}; //append new command here
+        var app = WebApplication.Create();
+        public void main() 
+        {
+            Console.Writeline("Welcome to the C# web application to search github!");
+            Console.Writeline("Q to quit, -r (repo) to search repos, -u (user) to search users!");
+            string response = Console.ReadLine(); 
+        }
+        public void Inputs()
+        {
+            main();
+            try()
+            {
+                if(response == "Q")
+                {
+                    //close application
+                }
+                else if(response == "-r")
+                {
+                    //github search repo
+                }
+                else if(response == "-u")
+                {
+                    //github search users
+                }
+            }
+            catch(response != responses)
+            {
+                Console.Writeline("Response not in commands!");
+                main();
+            }
+        }
+        app.Run();
+    }
 }
